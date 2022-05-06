4b3d9248bd96791c36804b67cb28d25860ac19a5
Geovany Vasquez <gvasquezp5@miumg.edu.gt>
2022-05-05 21:42:57 -0600

    segundoparcialhash

---
+++ src/segundo_parcial_hash/Segundo_parcial_hash.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package segundo_parcial_hash;
+
+/**
+ *
+ * @author Geovany
+ */
+public class Segundo_parcial_hash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_tablashash T = new frm_tablashash();
+        T.show();
+    }
+    
+}
